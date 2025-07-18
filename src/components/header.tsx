8f6e31386efed3590891a45773232ce5f976bdac
Ashish Gupta <aashish.gupta.mails@gmail.com>
2025-07-18 22:35:19 +0000

    Let's try playstore logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,8 +13,8 @@ const AppLogo = () => (
       xmlns="http://www.w3.org/2000/svg"
       className="text-primary"
     >
-      <rect width="32" height="32" rx="8" fill="currentColor"/>
-      <path d="M12 22V10H14.4V14.4H18.6C20.4778 14.4 22 15.9222 22 17.8C22 19.6778 20.4778 21.2 18.6 21.2H14.4V22H12ZM14.4 19.2H18.6C19.3719 19.2 20 18.5719 20 17.8C20 17.0281 19.3719 16.4 18.6 16.4H14.4V19.2Z" fill="#FFFFFF" className="dark:fill-white fill-primary-foreground"/>
+      <path d="M6 4C4.89543 4 4 4.89543 4 6V26C4 27.1046 4.89543 28 6 28H26C27.1046 28 28 27.1046 28 26V6C28 4.89543 27.1046 4 26 4H6Z" fill="currentColor"/>
+      <path d="M11.5366 22.4635L19.5 16.0001L11.5366 9.53674V22.4635Z" fill="#FFFFFF" className="dark:fill-white fill-primary-foreground"/>
     </svg>
     <span className="text-xl font-bold text-foreground">EarnByApps</span>
   </Link>
