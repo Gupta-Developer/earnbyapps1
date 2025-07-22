4e49cb26ee27b6c0963d12d7849cd66717e457b2
Ashish Gupta <aashish.gupta.mails@gmail.com>
2025-07-22 13:01:09 +0000

    ...

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -5,6 +5,7 @@ import type { ReactNode } from "react";
 import BottomNav from "@/components/bottom-nav";
 import Header from "@/components/header";
 import { AuthProvider } from "@/hooks/use-auth";
+import Footer from "@/components/footer";
 
 export default function UserLayout({ children }: { children: ReactNode }) {
   return (
@@ -13,6 +14,7 @@ export default function UserLayout({ children }: { children: ReactNode }) {
         <div className="w-full max-w-[480px] min-h-screen bg-card shadow-lg relative flex flex-col">
           <Header />
           <main className="flex-grow pt-16 pb-20">{children}</main>
+          <Footer />
           <BottomNav />
         </div>
       </div>
