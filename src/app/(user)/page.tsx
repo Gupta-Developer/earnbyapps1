005128e5e87ba99b28c60a6dbaf556284c84fa4c
Ashish Gupta <aashish.gupta.mails@gmail.com>
2025-08-10 15:36:34 +0000

    Tablist is kind of totally invisible where as tablist items are doing go

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -103,7 +103,7 @@ export default function HomePage() {
 
       <Tabs defaultValue={filter} onValueChange={setFilter} className="w-full">
         <div className="flex justify-center">
-            <TabsList className="bg-card">
+            <TabsList>
                 <TabsTrigger value="all">All Apps</TabsTrigger>
                 <TabsTrigger value="high-paying">High Paying</TabsTrigger>
                 <TabsTrigger value="instant">Instant Paying</TabsTrigger>
